c5286aa48e17bbf649ec3ad995eeeebaee500f8c
GodaProjects <goda.projects@gmail.com>
2021-02-23 13:28:28 +0530

    Slotted style applying only to the slots and not the shadow dom. Slot dom is also called light dom

--- slots/src/index.js
+++ slots/src/index.js
@@ -16,12 +16,18 @@ class MyApp extends LitElement {
             .content {
                 grid-area: "content"
             }
+
+            ::slotted(img) {
+                background-color: orange;
+                padding: 10px;
+            }
         </style>
         <div class="content">
             <div class="header">
                 Header
                 <slot></slot>
                 <slot name="named-slot"></slot>
+                <img src="../images/GTY_baby_deer_jt_140802_16x9_1600.jpg"/>
             </div>
             <div class="container">
                 Container
